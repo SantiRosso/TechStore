86d4727c5b467a71d1a3fa7b79c9e4e5de03befb
SantiRosso <santiagorosso@live.com.ar>
2023-08-17 20:20:20 -0300

    Navbar

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Navbar from "@/components/Navbar";
+
 export const metadata = {
   title: "TechStore",
   description: "Best tech store!",
@@ -6,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const Navbar = () => {
+    return(
+        <nav>
+            <h1>TechStore</h1>
+            <ul>
+                <li><Link href="/"><h5>Home</h5></Link></li>
+                <li><Link href="/about"><h5>About</h5></Link></li>
+                <li><Link href="/contact"><h5>Contact</h5></Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
