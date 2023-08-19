ff9e1c33fdd7d3a86a88118d1ad1af88c07eddbe
SantiRosso <santiagorosso@live.com.ar>
2023-08-19 17:38:03 -0300

    tailwind css

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
     return(
         <nav>
-            <h1>TechStore</h1>
+            <h1 className="text-3xl font-bold">TechStore</h1>
             <ul>
                 <li><Link href="/"><h5>Home</h5></Link></li>
                 <li><Link href="/about"><h5>About</h5></Link></li>
