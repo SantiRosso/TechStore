71c66aa0b78d62dbc3aea128107a0a78f00d5afe
SantiRosso <santiagorosso@live.com.ar>
2023-08-24 13:45:13 -0300

    search params and request body

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -6,12 +6,23 @@ const fetchUsers = async () => {
   return data.data;
 };
 
-export const GET = async () => {
+export const GET = async (request, { params }) => {
+  //search params
+  // console.log(request);
+  // const { searchParams } = new URL(request.url);
+  // console.log(searchParams);
+  // const first_name = searchParams.get("first_name");
+  // const last_name = searchParams.get("last_name");
+  // console.log(first_name, last_name);
+
   const users = await fetchUsers();
   return NextResponse.json(users);
 };
 
-export const POST = () => {
+export const POST = async (request) => {
+  //request body
+  // const data = await request.json();
+  // console.log(data);
   return NextResponse.json({ users: "Esto es un POST de 'users'." });
 };
 
